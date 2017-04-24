21a5235fb6659def317c9aeaf6f9d6e6fe200323
Dan Balaban <dan.stefan.balaban@gmail.com>
2017-04-24 22:53:39 +0300

    Display all audio files on device

--- app/src/main/java/me/dsbalaban/musicplayer/MainActivity.java
+++ app/src/main/java/me/dsbalaban/musicplayer/MainActivity.java
@@ -1,13 +1,73 @@
 package me.dsbalaban.musicplayer;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import android.net.Uri;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.widget.ListView;
+import android.provider.MediaStore.Audio;
+
 public class MainActivity extends Activity {
+    private ArrayList<Song> songList;
+    private ListView songView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+
+                // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
+                // app-defined int constant
+
+                return;
+            }
+        }
+
+        songView = (ListView)findViewById(R.id.song_list);
+        songList = new ArrayList<>();
+
+        getSongList();
+
+        Collections.sort(songList, new Comparator<Song>() {
+            public int compare(Song a, Song b) {
+                return a.getTitle().compareTo(b.getTitle());
+            }
+        });
+
+        SongAdapter songAdapter = new SongAdapter(this, songList);
+        songView.setAdapter(songAdapter);
+    }
+
+    public void getSongList() {
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        if (musicCursor != null && musicCursor.moveToFirst()) {
+            int titleColumn = musicCursor.getColumnIndex(Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex(Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex(Audio.Media.ARTIST);
+
+            do {
+                long currentId = musicCursor.getLong(idColumn);
+                String currentTitle = musicCursor.getString(titleColumn);
+                String currentArtist = musicCursor.getString(artistColumn);
+
+                songList.add(new Song(currentId, currentTitle, currentArtist));
+            } while (musicCursor.moveToNext());
+        }
     }
 }
